4b73b5b0701affabcf7edab29e6b9e25667cc0fd
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-01-27 17:09:39 -0600

    Initial Commit- Added Component Folder Structure and Files, database.sql information, and updated pool.js with db name

--- database.sql
+++ database.sql
@@ -1,10 +1,41 @@
+-- create new database in Postico named "civicly"
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "User" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (255) NOT NULL,
+    "isAdmin" BOOLEAN,
+    "image_url" TEXT,
+    "upvotes" INT
 );
+
+CREATE TABLE "Notifications" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" SERIAL FOREIGN KEY REFERENCES "User"("id"),
+    "ticket_id" SERIAL FOREIGN KEY REFERENCES "Ticket"("id"),
+    "comments" TEXT,
+    "timestamp" TIMESTAMP,
+    "notification_status" TEXT
+);
+
+CREATE TABLE "Ticket" (
+    "id" BIGINT PRIMARY KEY,
+    "image_url" TEXT,
+    "description" TEXT,
+    "status" TEXT,
+    "date" TIMESTAMPTZ,
+    "upvotes" INT,
+    "category" TEXT,
+    "user_id" SERIAL FOREIGN KEY REFERENCES "User"("id"),
+    "anonymous" BOOLEAN,
+    "subcategory_id" SERIAL FOREIGN KEY REFERENCES "Subcategories"("id"),
+    "latitude" VARCHAR(255),
+    "longitude" VARCHAR(255)
+);
+
+CREATE TABLE "Subcategories" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255),
+    "category" INT(255)
+);
+
