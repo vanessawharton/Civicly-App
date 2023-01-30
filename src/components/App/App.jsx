50536c9ad793b6d0fe76fe53a05823635ee5ef29
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-01-30 15:42:10 -0600

    Added theme colors to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,9 +7,9 @@ import {
 } from 'react-router-dom';
 
 import { useDispatch, useSelector } from 'react-redux';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 import Nav from '../User/NavMenu/NavMenu';
-import Footer from '../User/Footer/Footer';
 import Header from '../User/Header/Header';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
@@ -25,10 +25,6 @@ import TopCitizensPage from '../User/TopCitizensPage/TopCitizensPage';
 import UserProfilePage from '../User/UserProfilePage/UserProfilePage';
 import UserReportsPage from '../User/UserReportsPage/UserReportsPage';
 
-
-
-
-
 import './App.css';
 
 function App() {
@@ -40,138 +36,121 @@ function App() {
     dispatch({ type: 'FETCH_USER' });
   }, [dispatch]);
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1C67F6',
+        dark: '#2945d7',
+        light: '#8cd1ff',
+      },
+      secondary: {
+        main: '#FFBC00',
+        bright: '#FF5C3A',
+        neutral: '#FFA26B',
+      },
+      custom: {
+        main: '#1cd5f6',
+        purple: '#9800f3',
+        bright: '#f61c68',
+      }
+    },
+  });
+
   return (
-    <Router>
-      <div>
-        <Header />
-        <Nav />
-        <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Header />
+          <Nav />
+          <Switch>
+            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page.
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-          </Route> */}
+            <Route
+              exact
+              path="/login"
+            >
+              {user.id ?
+                // If the user is already logged in, 
+                // redirect to the /home page
+                <Redirect to="/home" />
+                :
+                // Otherwise, show the login page
+                <UserLoginPage />
+              }
+            </Route>
 
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          {/* <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-          </ProtectedRoute> */}
+            <Route
+              exact
+              path="/register"
+            >
+              <UserRegisterPage />
+            </Route>
 
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /home page
-              <Redirect to="/home" />
-              :
-              // Otherwise, show the login page
-              <UserLoginPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/register"
-          >
-            <UserRegisterPage />
-          </Route>
-
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /home page
-              <Redirect to="/home" />
-              :
-              // Otherwise, show the Landing page
+            <Route
+              exact
+              path="/home"
+            >
               <LandingPage />
-            }
-          </Route>
+            </Route>
 
-          <ProtectedRoute
-            exact path="/admindashboard"
-          >
-            <AdminDashboardPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/admindashboard"
+            >
+              <AdminDashboardPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/adminloginpage"
-          >
-            <AdminLoginPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/adminloginpage"
+            >
+              <AdminLoginPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/reportdetail/:id"
-          >
-            <ReportDetailView />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/reportdetail/:id"
+            >
+              <ReportDetailView />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/addreport"
-          >
-            <CreateReportPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/addreport"
+            >
+              <CreateReportPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/reportmap"
-          >
-            <MapViewPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/reportmap"
+            >
+              <MapViewPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/topcitizens"
-          >
-            <TopCitizensPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/topcitizens"
+            >
+              <TopCitizensPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/login"
-          >
-            <UserLoginPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/profile"
+            >
+              <UserProfilePage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            exact path="/profile"
-          >
-            <UserProfilePage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            exact path="/register"
-          >
-            <UserRegisterPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            exact path="/myreports"
-          >
-            <UserReportsPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              exact path="/myreports"
+            >
+              <UserReportsPage />
+            </ProtectedRoute>
 
 
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>404</h1>
-          </Route>
-        </Switch>
-        {/* <Footer /> */}
-      </div>
-    </Router>
+            {/* If none of the other routes matched, we will show a 404. */}
+            <Route>
+              <h1>404</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
