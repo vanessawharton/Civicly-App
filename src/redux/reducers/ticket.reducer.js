a54d4deb9d7b0b23a4d20ff50686d2cc5eac64ad
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-01-30 16:16:53 -0600

    finished ticket reducer

---
+++ src/redux/reducers/ticket.reducer.js
@@ -0,0 +1,13 @@
+// Not sure if the state needs to be an Object or Array for this reducer. I figured it will be an object with multiple values. If someone could explain this I would appreciate it.
+//                           ~~~V~~~
+const ticketReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'SET_TICKET':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+
+export default ticketReducer;
