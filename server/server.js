d34f2d98b77e34e253105f8d0a5b021d16c99608
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-01-30 11:05:26 -0600

    notifications router finished

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const ticketRouter = require('./routes/ticket.router');
+const notificationsRouter = require('./routes/notifications.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -17,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Passport Session Configuration //
 app.use(sessionMiddleware);
-
+git
 // start up passport sessions
 app.use(passport.initialize());
 app.use(passport.session());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/ticket', ticketRouter);
+app.use('/api/notifications', notificationsRouter);
 
 // Serve static files
 app.use(express.static('build'));
