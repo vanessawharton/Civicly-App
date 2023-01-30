b2743f34006c6add4aff361854f7dcb12084f83e
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-01-30 13:46:16 -0600

    added ticket saga and connected to root saga

---
+++ src/redux/sagas/ticket.saga.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+
+function* fetchTicket() {
+    try {
+        const response = yield axios.get('/api/ticket');
+
+        yield put({ type: 'SET_USER', payload: response.data });
+    } catch (error) {
+        console.log('Ticket GET request failed', error);
+    }
+}
+
+
+
+
+function* ticketSaga() {
+    yield takeLatest('FETCH_TICKET', fetchTicket);
+
+}
+
+export default ticketSaga;
