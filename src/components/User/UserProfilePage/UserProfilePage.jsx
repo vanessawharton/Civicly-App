8db50bbacc7e5e59fc5110c530f4de32432cc0d8
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-01-31 17:00:01 -0600

    added avatar/profile image and edit button icon

--- src/components/User/UserProfilePage/UserProfilePage.jsx
+++ src/components/User/UserProfilePage/UserProfilePage.jsx
@@ -1,5 +1,45 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+
+
 export default function UserProfilePage() {
+    
+    
+    
     return (
-        <h1>User Profile Page</h1>
+        <>
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+            <Typography variant="h4" gutterBottom>
+                Citizen Profile
+            </Typography>
+            <IconButton color="black" size="medium" aria-label="edit profile" component="label">
+                <EditOutlinedIcon fontSize="large"/>
+            </IconButton>
+            </Box>
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Stack direction="row" spacing={2}>
+                    <Avatar
+                    alt="Profile Image"
+                    src=""
+                    sx={{ width: 200, height: 200 }}
+                    />
+                </Stack>
+            </Box>
+        </>
     )
 }
