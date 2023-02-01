f0581997865a1f96b2d236a98643ba021274746b
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-01-31 23:13:31 -0600

    finished base styling for user profile view

--- src/components/User/UserProfilePage/UserProfilePage.jsx
+++ src/components/User/UserProfilePage/UserProfilePage.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
@@ -6,12 +7,18 @@ import Typography from '@mui/material/Typography';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { fontFamily, fontSize } from '@mui/system';
 
+// ~~~ WORK IN PROGRESS ~~~
 
+// Styled somewhat but needs a lot of work still
+// Also My Reports href is ready for button push
+// Need to work on align edit icon to middle as well as further right
+// Center Citizen
+// Research best way to pass data to typography
+// Refactor and indent
 
 export default function UserProfilePage() {
-    
+    const dispatch = useDispatch();
     
     
     return (
@@ -19,15 +26,15 @@ export default function UserProfilePage() {
             <Box sx={{
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: 'center',
+                justifyContent: 'center'
             }}>
-            <Typography variant="h4" gutterBottom>
-                Citizen Profile
-            </Typography>
-            <IconButton color="black" size="medium" aria-label="edit profile" component="label">
-                <EditOutlinedIcon fontSize="large"/>
-            </IconButton>
-            </Box>
+                <Typography variant="h4" gutterBottom>
+                    Citizen Profile
+                </Typography>
+                    <IconButton color="black" size="medium" aria-label="edit profile" component="label">
+                        <EditOutlinedIcon fontSize="large"/>
+                    </IconButton>
+            </Box><br></br>
             <Box sx={{
                 display: 'flex',
                 alignItems: 'center',
@@ -49,6 +56,42 @@ export default function UserProfilePage() {
                 fontSize: 'h5.fontSize'
             }}>
                 username
+            </Box><br></br><br></br>
+            <Box component="span" sx={{ 
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>Reports Submitted: 00
+            </Box><br></br>
+            <Box component="span" sx={{ 
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>Upvotes: 00
+            </Box><br></br><br></br><br></br>
+            <Box component="span" sx={{ 
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>Home City: St. Paul, MN
+            </Box><br></br>
+            <Box component="span" sx={{ 
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>
+            <Button sx={{ width: 130, padding: 1, margin: 1 }} color="secondary" href="/topcitizens#/topcitizens" variant="contained">
+                My Reports
+            </Button>
+            </Box>
+            <Box component="span" sx={{ 
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>
+            <Button  sx={{ width: 130, padding: 1, margin: 1 }} color="primary" href="/" variant="contained" onClick={() => dispatch({ type: 'LOGOUT' })}>
+                Log Out
+            </Button>
             </Box>
         </>
     )
