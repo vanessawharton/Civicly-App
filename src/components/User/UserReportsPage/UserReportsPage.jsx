d2cde1465e11087184cc88114d1c2df31e3abf60
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-02-01 17:01:37 -0600

    added reducers to userProfile and myReports

--- src/components/User/UserReportsPage/UserReportsPage.jsx
+++ src/components/User/UserReportsPage/UserReportsPage.jsx
@@ -1,5 +1,40 @@
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { useEffect } from 'react';
+import { useSelector } from "react-redux";
+
+
+
 export default function UserReportsPage() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const users = useSelector((store) => store.user);
+
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+        dispatch({ type: 'FETCH_ALL_TICKETS' })
+    }, []);
+
     return (
-        <h1>User Reports Page</h1>
+        <>
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: 2,
+                fontWeight: 'bold',
+                fontSize: 'h5.fontSize'
+            }}>
+                {users.username}'s Reports
+            </Box>
+            <Divider sx={{margin: 3}}/>
+        
+        
+        
+        </>
     )
 }
