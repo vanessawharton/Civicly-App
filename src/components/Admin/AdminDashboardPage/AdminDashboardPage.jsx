002868dc968e9e1d9a70257b554123896be3be44
Jared Lindquist <jared.lindquist1985@gmail.com>
2023-02-06 11:48:06 -0600

    added header into AdminDashboardPage component.

--- src/components/Admin/AdminDashboardPage/AdminDashboardPage.jsx
+++ src/components/Admin/AdminDashboardPage/AdminDashboardPage.jsx
@@ -81,7 +81,7 @@ export default function AdminDashBoardPage() {
 
     return (
         <>
-        
+            <Header /><br/><br/>
             <div className="body">
                 
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 10 }}
