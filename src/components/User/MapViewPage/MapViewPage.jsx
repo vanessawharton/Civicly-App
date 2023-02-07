2de0c2f5eb58960eb48457a9c43c01a9be271de3
Paul Norling <paulnorling87@gmail.com>
2023-02-07 10:36:52 -0600

    ReportsDetailMap

--- src/components/User/MapViewPage/MapViewPage.jsx
+++ src/components/User/MapViewPage/MapViewPage.jsx
@@ -1,13 +1,13 @@
 import ReportsMap from "./ReportsMap";
 import Nav from '../NavMenu/NavMenu';
 import Header from '../Header/Header';
-import ReportDetailMap from "../../Admin/ReportDetailMap/ReportDetailMap";
+
 
 function MapViewPage() {
   return (
     <>
       <Header />
-      <ReportDetailMap/>
+      <ReportsMap/>
       <Nav />
     </>
   )
