f9f5c5e086408e5d7c9eb114d87341145f72b5a3
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-02-08 16:29:07 -0600

    Re-routed by user logged in, isAdmin, or none. Added Logout button for Admin Dashboard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,8 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
+import NavMenu from '../User/NavMenu/NavMenu';
+import Header from '../User/Header/Header';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import LandingPage from '../User/LandingPage/LandingPage';
 import HomePage from '../User/HomePage/HomePage';
@@ -58,108 +60,183 @@ function App() {
     <LoadScript
       googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
     <ThemeProvider theme={theme}>
-      <Router>
-        <div>
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
+      {user.id ?
+        <div id="logged-in-user">
+        {user.isAdmin === true ?
+          <div id="admin-user">
+            <Router>
+              <div>
+                <Switch>
+                  <Redirect exact from="/admin" to="/admin/dashboard" />
+      
+                  <Redirect exact from="/admin/login" to="/admin/dashboard" />
 
-            <Route
-              exact
-              path="/home"
-            >
-              {user.id ?
-                // If the user is already logged in, 
-                // redirect to the /home page
-                <HomePage />
-                :
-                // Otherwise, show the login page
-                <LandingPage />
-              }
-            </Route>
-
-            <Route
-              exact
-              path="/login"
-            >
-              {user.id ?
-                // If the user is already logged in, 
-                // redirect to the /home page
-                <Redirect to="/home" />
-                :
-                // Otherwise, show the login page
-                <UserLoginPage />
-              }
-            </Route>
-
-            <Route
-              exact
-              path="/register"
-            >
-              {user.id ?
-                // If the user is already logged in, 
-                // redirect to the /home page
-                <Redirect to="/home" />
-                :
-                // Otherwise, show the login page
-                <UserRegisterPage />
-              }
-            </Route>
-
-            <Redirect exact from="/admin" to="/admin/dashboard" />
-
-            <Route
-              exact path="/admin/dashboard"
-            >
-              <AdminDashboardPage />
-            </Route>
-
-            <Route
-              exact path="/admin/login"
-            >
-              <AdminLoginPage />
-            </Route>
-
-            <ProtectedRoute
-              exact path="/addreport"
-            >
-              <CreateReportPage />
-            </ProtectedRoute>
-
-            <ProtectedRoute
-              exact path="/reportmap"
-            >
-              <MapViewPage />
-            </ProtectedRoute>
-
-            <ProtectedRoute
-              exact path="/topcitizens"
-            >
-              <TopCitizensPage />
-            </ProtectedRoute>
-
-            <ProtectedRoute
-              exact path="/profile"
-            >
-              <UserProfilePage />
-            </ProtectedRoute>
-
-            <ProtectedRoute
-              exact path="/myreports"
-            >
-              <UserReportsPage />
-            </ProtectedRoute>
-
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route>
-              <h1>404</h1>
-            </Route>
-          </Switch>
+                  <ProtectedRoute
+                    exact path="/admin/dashboard"
+                  >
+                    <AdminDashboardPage />
+                  </ProtectedRoute>
+      
+                  {/* If none of the other routes matched, we will show a 404. */}
+                  <Route>
+                    <h1>404</h1>
+                  </Route>
+                </Switch>
+              </div>
+            </Router>
+          </div>
+        :
+          <div id="non-admin-user">
+              <Router>
+                <div>
+                  <Header />
+                  <Switch>
+                    {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+                    <Redirect exact from="/" to="/home" />
+        
+                    <Route
+                      exact
+                      path="/home"
+                    >
+                      {user.id ?
+                        // If the user is already logged in, 
+                        // redirect to the /home page
+                        <HomePage />
+                        :
+                        // Otherwise, show the login page
+                        <LandingPage />
+                      }
+                    </Route>
+        
+                    <Route
+                      exact
+                      path="/login"
+                    >
+                      {user.id ?
+                        // If the user is already logged in, 
+                        // redirect to the /home page
+                        <Redirect to="/home" />
+                        :
+                        // Otherwise, show the login page
+                        <UserLoginPage />
+                      }
+                    </Route>
+        
+                    <Route
+                      exact
+                      path="/register"
+                    >
+                      {user.id ?
+                        // If the user is already logged in, 
+                        // redirect to the /home page
+                        <Redirect to="/home" />
+                        :
+                        // Otherwise, show the login page
+                        <UserRegisterPage />
+                      }
+                    </Route>
+        
+                    <ProtectedRoute
+                      exact path="/addreport"
+                    >
+                      <CreateReportPage />
+                    </ProtectedRoute>
+        
+                    <ProtectedRoute
+                      exact path="/reportmap"
+                    >
+                      <MapViewPage />
+                    </ProtectedRoute>
+        
+                    <ProtectedRoute
+                      exact path="/topcitizens"
+                    >
+                      <TopCitizensPage />
+                    </ProtectedRoute>
+        
+                    <ProtectedRoute
+                      exact path="/profile"
+                    >
+                      <UserProfilePage />
+                    </ProtectedRoute>
+        
+                    <ProtectedRoute
+                      exact path="/myreports"
+                    >
+                      <UserReportsPage />
+                    </ProtectedRoute>
+        
+                    {/* If none of the other routes matched, we will show a 404. */}
+                    <Route>
+                      <h1>404</h1>
+                    </Route>
+                  </Switch>
+                  <NavMenu />
+                </div>
+              </Router>
+          </div>
+        }
         </div>
-      </Router>
+      :
+        <div id="non-user">
+          <Router>
+            <div>
+              <Switch>
+                {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+                <Redirect exact from="/" to="/home" />
+    
+                <Route
+                  exact
+                  path="/home"
+                >
+                  <LandingPage />
+                </Route>
+    
+                <Route
+                  exact
+                  path="/login"
+                >
+                  <UserLoginPage />
+                </Route>
+    
+                <Route
+                  exact
+                  path="/register"
+                >
+                    <UserRegisterPage />
+                </Route>
+
+                <Redirect exact from="/admin" to="/admin/login" />
+
+                <Redirect exact from="/admin/dashboard" to="/admin/login" />
+      
+                <Route
+                  exact
+                  path="/admin/login"
+                >
+                  {user.id ?
+                    // If the admin is already logged in, 
+                    // redirect to the admin dashboard page
+                    <Redirect to="/admin/dashboard" />
+                    :
+                    // Otherwise, show the login page
+                    <AdminLoginPage />
+                  }
+                </Route>
+    
+                {/* If none of the other routes matched, we will show a 404. */}
+                <Route>
+                  <h1>404</h1>
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      }
     </ThemeProvider>
     </LoadScript>
   );
 }
+
 
 export default App;
