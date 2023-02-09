861c4ea523d97b29ed44a81bf2b9acbf3e64e4f2
Paul Norling <paulnorling87@gmail.com>
2023-02-09 10:30:57 -0600

    formating and infowindow styling

--- src/components/User/MapViewPage/ReportsMap.jsx
+++ src/components/User/MapViewPage/ReportsMap.jsx
@@ -4,26 +4,24 @@ import React, { useEffect, useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import Typography from '@mui/material/Typography';
 import { Box } from "@mui/system";
-import './ReportsMap.css'
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
+import './ReportsMap.css';
 import { Button } from "@mui/material";
 
 
 
 
 function ReportsMap(){
-  
+
+  const dispatch = useDispatch();
+
   const [latitude, setLatitude] = useState(44.9778);
   const [longitude, setLongitude] = useState(-93.2650);
   const [focus, setFocus] = useState(8);
   const [activeMarker, setActiveMarker] = useState(null);
   const [mapref, setMapRef] = useState(null);
-  
-  
-  const dispatch = useDispatch();
+  const [upvoteSelected, setUpvoteSelected] = useState(false);
 
+  
   const tickets = useSelector(store => store.ticket)
   const activeMarkerTriggerTicket = useSelector(store => store.activeMarker)
 
@@ -32,11 +30,7 @@ function ReportsMap(){
     dispatch({type: 'FETCH_ALL_TICKETS'});
     !activeMarkerTriggerTicket && userPosition()
 
-  }, [])
-
-  // useEffect(() => {
-  //   handleActiveMarker(activeMarkerTriggerTicket)
-  // }, [activeMarkerTriggerTicket] )
+  }, []);
 
   const userPosition = () => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -71,40 +65,35 @@ function ReportsMap(){
         ticket.latitude > bounds?.Ya.lo && 
         ticket.longitude < bounds?.Ma.hi && 
         ticket.longitude > bounds?.Ma.lo
-        )
-      }
-          
-      )
+      )})
       dispatch({type: 'SET_INBOUNDSMARKERS', payload: showTickets});
       }
-    };
+  };
 
   const handleActiveMarker = (marker) => {
     console.log(marker);
     setLatitude(+marker.latitude);
     setLongitude(+marker.longitude);
-    setSelected(false)    
+    setUpvoteSelected(false)    
     if (marker.id === activeMarker) {
       return;
     }
       setActiveMarker(marker.id);
   };
 
-  const [selected, setSelected] = useState(false);
-
   const handleUpVote =(location) => {
-    setSelected(!selected)
-    if(selected == false){
+    setUpvoteSelected(!upvoteSelected)
+    if(upvoteSelected == false){
       dispatch({type: 'UPVOTE', payload: {location: location}})
     }
-    else if (selected == true){
+    else if (upvoteSelected == true){
       dispatch({type: 'DOWNVOTE', payload: {location: location}})
     } 
   } 
 
   const onCloseClick = () => {
     setActiveMarker(null)
-    setSelected(false)
+    setUpvoteSelected(false)
   }
 
   const defaultMapOptions = {
@@ -114,107 +103,103 @@ function ReportsMap(){
   };
  
     return (
-     <>
-      <GoogleMap 
-        onZoomChanged = {handleCenterChanged}
-        onDragEnd = {handleCenterChanged}
-        onCenterChanged = {handleCenterChanged}
-        onLoad={handleMapLoad}
-        zoom={focus} 
-        center={{lat: latitude, lng: longitude}}
-        mapContainerClassName="map-container"
-        onClick={() => onCloseClick()}
-        options={defaultMapOptions}
-      >
-      {tickets.map(location => {
+      <>
+        <GoogleMap 
+          onZoomChanged = {handleCenterChanged}
+          onDragEnd = {handleCenterChanged}
+          onCenterChanged = {handleCenterChanged}
+          onLoad={handleMapLoad}
+          zoom={focus} 
+          center={{lat: latitude, lng: longitude}}
+          mapContainerClassName="map-container"
+          onClick={() => onCloseClick()}
+          options={defaultMapOptions}
+        >
+        {tickets.map(location => {
 
-        let color;
-        
-        //let category;
-        
-        switch(location.category) {
-          case '8':
-            color='/images/yellow-dot.png'
-            //category='Sidewalks and Streets'
-            break;
-          case '7':
-            color='/images/red-dot.png'
-            //category='Property'
-            break;
-          case '6':
-            color='/images/blue-dot.png'
-            //category='Parking'
-            break;
-          case '5':
-            color='/images/green-dot.png'
-            //category='Health and Environmental'
-            break;
-          case '4':
-            color='/images/marker_grey.png'
-            //category='Graffiti'
-            break;
-          case '3':
-            color='/images/pink-dot.png'
-            //category='Garbage and Recycling'
-            break;
-          case '2':
-            color='/images/marker_brown.png'
-            //category='Biking'
-            break;
-          case '1':
-            color='/images/orange-dot.png'
-            //category='Animal Control'
-            break;
-          case '0':
-            color='/images/purple-dot.png'
-            //category='Accessibility'
-            break;
-          default:
-            color='/images/red-dot.png'
-        }
-        return (
-       
-            <MarkerF key ={location.id} position={{lat: +location.latitude, lng: +location.longitude}} onClick={() => handleActiveMarker(location)} 
-            options={{icon: `${color}`}}
-            >
-            {activeMarker === location.id ? (
-              <InfoWindowF onCloseClick={onCloseClick}>
-                <div>
-              <Typography sx={{fontSize: 14, fontWeight: 700, mb: 1, textAlign: "center"}}>{location.categoryName}</Typography>
-              <div className="infoWindowContent">
-                <Box className="textContainer">
-                  {/* <Typography sx={{fontSize: 12, fontWeight: 700, mb: 1}}>{location.categoryName}</Typography> */}
-                  <Typography sx={{fontSize: 10}}>
-                    <Typography sx={{fontWeight: 700, fontSize: 10}}> Reported:
-                    </Typography> 
-                       {new Date(location.date).toLocaleDateString()}
-                  </Typography>
-                  <Typography sx={{fontSize: 10}}><Typography sx={{fontWeight: 700, fontSize: 10}}>Status:</Typography> {location.status}</Typography>
-                  <Box sx={{ display: 'flex' , flexDirection: 'row', mt: 1}}>
-                    {/* <Tooltip>
-                      <IconButton>
-                        <ThumbUpIcon selected={selected} sx={{fontSize: 20, color: '#4c8bf5'}} onClick={() => handleUpVote(location)}/>
-                      </IconButton>
-                    </Tooltip> */}
-                    <Button onClick={() => handleUpVote(location)} sx={{ml: -1.5, mt: 2, fontSize: 10, color: 'grey'}}>upvote</Button>
-                    <Typography sx={{fontSize: 10, mt: 3}}>
-                      {location.upvotes}
-                    </Typography>
-                  </Box>
-                </Box>
-                <img className="infoWindow-image" src={location.image_url}/>
-              </div>
-              </div>
-            </InfoWindowF>
-            ) : null}
-            </MarkerF>
+          let color;
           
-        )
-      })}
-      </GoogleMap>
-      <ReportCarousel handleActiveMarker={handleActiveMarker} />
+          switch(location.category) {
+            case '8':
+              color='/images/yellow-dot.png'
+              break;
+            case '7':
+              color='/images/red-dot.png'
+              break;
+            case '6':
+              color='/images/blue-dot.png'
+              break;
+            case '5':
+              color='/images/green-dot.png'
+              break;
+            case '4':
+              color='/images/marker_grey.png'
+              break;
+            case '3':
+              color='/images/pink-dot.png'
+              break;
+            case '2':
+              color='/images/marker_brown.png'
+              break;
+            case '1':
+              color='/images/orange-dot.png'
+              break;
+            case '0':
+              color='/images/purple-dot.png'
+              break;
+            default:
+              color='/images/red-dot.png'
+          }
+
+          return (
+        
+              <MarkerF 
+                key ={location.id} 
+                position={{lat: +location.latitude, lng: +location.longitude}} 
+                onClick={() => handleActiveMarker(location)} 
+                options={{icon: `${color}`}}
+              >
+              {activeMarker === location.id ? (
+                <InfoWindowF onCloseClick={onCloseClick}>
+                  <div>
+                    <Typography sx={{fontSize: 14, fontWeight: 700, mb: 1, textAlign: "center"}}>
+                      {location.categoryName}
+                    </Typography>
+                    <div className="infoWindowContent">
+                      <Box >
+                        <Typography sx={{fontSize: 10}}>
+                          <Typography sx={{fontWeight: 700, fontSize: 10}}> 
+                            Reported:
+                          </Typography> 
+                            {new Date(location.date).toLocaleDateString()}
+                        </Typography>
+                        <Typography sx={{fontSize: 10}}>
+                          <Typography sx={{fontWeight: 700, fontSize: 10}}>
+                            Status:
+                          </Typography> 
+                            {location.status}
+                        </Typography>
+                        <Box sx={{ display: 'flex' , flexDirection: 'row', mt: 1}}>
+                          <Button onClick={() => handleUpVote(location)} sx={{ml: -1.5, mt: 2, fontSize: 10, color: 'grey'}}>
+                            upvote
+                          </Button>
+                          <Typography sx={{fontSize: 10, mt: 3}}>
+                            {location.upvotes}
+                          </Typography>
+                        </Box>
+                      </Box>
+                      <img className="infoWindow-image" src={location.image_url}/>
+                    </div>
+                  </div>
+                </InfoWindowF>
+              ) : null}
+              </MarkerF>
+          )
+        })}
+        </GoogleMap>
+        <ReportCarousel handleActiveMarker={handleActiveMarker} />
       </>
-      )
-}
+    )
+};
 
 export default ReportsMap;
