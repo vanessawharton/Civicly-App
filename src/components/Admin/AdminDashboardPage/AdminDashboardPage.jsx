59d74c3ab631470e1a04462279c960132071936e
Jared Lindquist <jared.lindquist1985@gmail.com>
2023-02-15 11:57:22 -0600

    more deletion of unused functions/code

--- src/components/Admin/AdminDashboardPage/AdminDashboardPage.jsx
+++ src/components/Admin/AdminDashboardPage/AdminDashboardPage.jsx
@@ -1,64 +1,25 @@
 import AdminHeader from "../AdminHeader/AdminHeader";
 import AdminDataTable from "./AdminDataTable";
 import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
-// import useReduxStore from '../../hooks/useReduxStore';
-import { Grid, TextField } from "@mui/material"
-import { useHistory, useParams } from 'react-router-dom';
-import { Button, InputLabel, FormControl, NativeSelect } from '@mui/material';
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { useEffect } from 'react';
+import { Grid } from "@mui/material"
 import './AdminDashboardPage.css';
-import { useSelector } from "react-redux";
 
 
 export default function AdminDashBoardPage() {
-
-    const { id } = useParams();
-    const history = useHistory();
-    // const store = useReduxStore();
     const dispatch = useDispatch();
-    const [reportNumber, setReportNumber] = useState();
-    const [status, setStatus] = useState();
-    const [category, setCategory] = useState();
-    const [subcategory, setSubcategory] = useState();
-    const [location, setLocation] = useState();
-    const [submittedBy, setSubmittedBy] = useState();
-    const [date, setDate] = useState();
-    const [notes, setNotes] = useState();
-    const [license, setLicense] = useState();
-    const [lastStatusUpdate, setLastStatusUpdate] = useState();
-    const [internalComments, setInternalComments] = useState();
-    const [image, setImage] = useState();
-    const [open, setOpen] = useState(false);
-
-    const tickets = useSelector((store) => store.ticket);
 
     //useEffect to Fetch all tickets from DB on page load
     useEffect(() => {
         // window.scrollTo(0, 0)
         dispatch({ type: 'FETCH_ALL_TICKETS' })
     }, []);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    }
-
-    const confirmClose = () => {
-        console.log('confirm close report button clicked');
-        //need to add either a sweetalert or another mui dialog for confirmation
-    }
-
     // console.log(tickets);
 
     return (
         <>
-            <AdminHeader /><br/><br/>
+            <AdminHeader /><br /><br />
             <div className="body">
-                
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 10 }}
                     display="flex"
                     justifyContent="center"
@@ -80,131 +41,10 @@ export default function AdminDashBoardPage() {
                         alignItems="center"
                         marginLeft={5}
                         marginRight={5}>
-                        <AdminDataTable /><br/><br/>
+                        <AdminDataTable /><br /><br />
                     </Grid>
                 </Grid>
-                <Dialog open={open} onClose={handleClose}>
-                    <DialogTitle>Report Details</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Below you will find the details for this report
-                        </DialogContentText>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Report Number"
-                            value={reportNumber}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Status"
-                            value={status}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Category"
-                            value={category}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Subcategory"
-                            value={subcategory}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Location"
-                            value={location}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Submitted By"
-                            value={submittedBy}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Date"
-                            value={date}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Notes"
-                            value={notes}
-                            type="text"
-                            fullWidth
-                            multiline
-                            maxRows={10}
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Last Status Update"
-                            value={lastStatusUpdate}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Internal Comments"
-                            value={internalComments}
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button style={{ backgroundColor: "#5A5A5A" }}
-                            variant="contained"
-                            onClick={handleClose}
-                        >Back To Dashboard</Button>
-                        <Button style={{ backgroundColor: "#bf0000" }}
-                            variant="contained"
-                            onClick={confirmClose}
-                        >Close Report</Button>
-                    </DialogActions>
-                </Dialog>
             </div>
-
         </>
     )
 }
